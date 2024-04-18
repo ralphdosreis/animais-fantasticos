a9541200be894cae0f481086111c71e2eaccdc25
ralphdosreis <ralphdosreistorres@gmail.com>
2024-04-18 17:42:14 -0300

    First commit

---
+++ js/modules/funcionamento.js
@@ -0,0 +1,30 @@
+export default function initFuncionamento() {
+  const funcionamento = document.querySelector("[data-semana]");
+  const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
+  const horarioSemana = funcionamento.dataset.horario.split(",").map(Number);
+
+  const dataAgora = new Date();
+  const diaAgora = dataAgora.getDay();
+  const horarioAgora = dataAgora.getHours();
+
+  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+  const horarioAberto =
+    horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
+  if (semanaAberto && horarioAberto) {
+    funcionamento.classList.add("aberto");
+  }
+}
+
+// const agora = new Date();
+// const futuro = new Date("May 8 2024 16:42");
+// console.log(agora);
+// console.log(futuro);
+// console.log(agora.getMonth());
+
+// function transformarDias(tempo) {
+//   return tempo / (24 * 60 * 60 * 1000);
+// }
+
+// const diasAgora = transformarDias(agora.getTime());
+// const diasFuturo = transformarDias(futuro.getTime());
+// console.log(Math.floor(diasFuturo - diasAgora));
